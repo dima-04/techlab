412cf7b2b2a75c55168043f9844b70e81a6416d7
Dima <dima.dibb1@gmail.com>
2020-01-06 22:20:12 -0800

    get articls

--- server.js
+++ server.js
@@ -2,82 +2,76 @@ var express = require("express");
 var cheerio = require("cheerio");
 var mongojs = require("mongojs");
 
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+
 var databaseUrl = "techlab";
 var collections = ["articles"];
 var db = mongojs(databaseUrl, collections);
 
 // Log any mongojs errors to console
-db.on("error", function(error) {
+db.on("error", function (error) {
   console.log("Database Error:", error);
 });
+
 // Makes HTTP request for HTML page
 var axios = require("axios");
-axios.get("https://www.nytimes.com/section/technology").then(function(response) {
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(response.data);
+app.post("/api/articles", function (req, res) {
+  axios.get("https://www.nytimes.com/section/technology").then(function (response) {
 
-  // An empty array to save the data that we'll scrape
-  var results = [];
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(response.data);
 
-  // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
-  $("li.css-ye6x8s").each(function(i, element) {
+    // An empty array to save the data that we'll scrape
+    var results = [];
 
-    // Save the text of the element in a "title" variable
-    var title = $(element).find("h2").text();
-    var summary = $(element).find("p").text();
+    // With cheerio, find each p-tag with the "title" class
+    // (i: iterator. element: the current element)
+    $("li.css-ye6x8s").each(function (i, element) {
 
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).find("a").attr("href");
+      // Save the text of the element in a "title" variable
+      var title = $(element).find("h2").text();
+      var summary = $(element).find("p").text();
 
-    // Save these results in an object that we'll push into the results array we defined earlier
-   db.articles.save({
-      title: title,
-      link: link,
-      summary:summary
+      // In the currently selected element, look at its child elements (i.e., its a-tags),
+      // then save the values for any "href" attributes that the child elements may have
+      var link = $(element).find("a").attr("href");
+
+      // Save these results in an object that we'll push into the results array we defined earlier
+      db.articles.save({
+        title: title,
+        link: link,
+        summary: summary
+      });
     });
+    res.send("Articles Updated")
   });
-  db.articles.find().sort({ title: 1 }, function(error, found) {
+});
+app.get("/api/articles", function (req, res) {
+
+
+  db.articles.find().sort({ title: 1 }, function (error, found) {
     // Log any errors if the server encounters one
     if (error) {
       console.log(error);
+      res.send(error);
     }
     // Otherwise, send the result of this query to the browser
     else {
-      console.log(found);
+     res.send(found);
     }
   });
-
-  
-    
 });
 
 
-
-//   app.get("/", function(req, res) {
-//     res.send("Hello world");
-//   });
-  
-//   // 2. At the "/all" path, display every entry in the animals collection
-//   app.get("/all", function(req, res) {
-//     // Query: In our database, go to the animals collection, then "find" everything
-//     db.tech.find({}, function(error, found) {
-//       // Log any errors if the server encounters one
-//       if (error) {
-//         console.log(error);
-//       }
-//       // Otherwise, send the result of this query to the browser
-//       else {
-//         res.json(found);
-//       }
-//     });
-//   });
-
-//   // Log the results once you've looped through each of the elements found with cheerio
-  
-// app.listen(3000, function() {
-//   console.log("App running on port 3000!");
-// });
+app.listen(PORT, function () {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
+});
